9571a8ca8e9a4fe7e5db475af4ff4b0c1ee4f0a6
pokhengly <henglypok21012021@gmail.com>
2025-04-09 10:25:16 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Rean Web"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.brown,
+              ),
+              height: 100.0,
+              margin: EdgeInsets.all(5.0),
+            ),
+            Container(
+              height: 100.0,
+              color: Colors.blue,
+              margin: EdgeInsets.all(5.0),
+            ),
+            Container(
+              height: 100.0,
+              color: Colors.pink,
+              margin: EdgeInsets.all(5.0),
+            )
+          ],
+        )
+      )
+    )
+  );
+}
